4aa80739ed4dcee65506f2bcb7887947869607c8
Davies Liu <davies@juicedata.io>
2020-07-16 22:24:28 +0800

    Update object_storage_test.go

--- object/object_storage_test.go
+++ object/object_storage_test.go
@@ -70,15 +70,15 @@ func testStorage(t *testing.T, s ObjectStorage) {
 		t.Errorf("out-of-range get: 'o', but got %v, error:%s", len(d), e)
 	}
 
-	objs, err2 := listAll(s, "", "", 1)
+	objs, err2 := listAll(s, "", "/", 1)
 	if err2 == nil {
 		if len(objs) != 1 {
 			t.Fatalf("List should return 1 keys, but got %d", len(objs))
 		}
-		if objs[0].Key != "/test" && objs[0].Key != "/" /* special case for fileStore */ {
+		if objs[0].Key != "/test" {
 			t.Fatalf("First key should be /test, but got %s", objs[0].Key)
 		}
-		if !strings.Contains(s.String(), "encrypted") && objs[0].Size != 5 && objs[0].Size != 0 /* special case for fileStore */ {
+		if !strings.Contains(s.String(), "encrypted") && objs[0].Size != 5 {
 			t.Fatalf("Size of first key shold be 5, but got %v", objs[0].Size)
 		}
 		now := time.Now()
