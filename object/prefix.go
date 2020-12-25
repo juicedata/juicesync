45c9754588cb025f5eb6c0c0308e591fbd546cd3
Davies Liu <davies@juicedata.io>
2020-12-25 13:28:13 +0800

    add Create() to create new bucket

--- object/prefix.go
+++ object/prefix.go
@@ -23,6 +23,10 @@ func (p *withPrefix) String() string {
 	return fmt.Sprintf("%s/%s", p.os, p.prefix)
 }
 
+func (p *withPrefix) Create() error {
+	return p.os.Create()
+}
+
 func (p *withPrefix) Head(key string) (*Object, error) {
 	obj, err := p.os.Head(p.prefix + key)
 	if err != nil {
