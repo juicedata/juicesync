bf1362bb60faa9b5efd5eb5c6f4be26cf93e3ed6
Sandy Xu <sandy@juicedata.io>
2023-04-07 13:07:14 +0800

    make the special char '\' behavior consistent with aws s3

--- main.go
+++ main.go
@@ -178,8 +178,16 @@ func run(c *cli.Context) error {
 	utils.InitLoggers(false)
 
 	// Windows support `\` and `/` as its separator, Unix only use `/`
-	srcURL := strings.Replace(c.Args().Get(0), "\\", "/", -1)
-	dstURL := strings.Replace(c.Args().Get(1), "\\", "/", -1)
+	srcURL := c.Args().Get(0)
+	dstURL := c.Args().Get(1)
+	if runtime.GOOS == "windows" {
+		if !strings.Contains(srcURL, "://") {
+			srcURL = strings.Replace(srcURL, "\\", "/", -1)
+		}
+		if !strings.Contains(dstURL, "://") {
+			dstURL = strings.Replace(dstURL, "\\", "/", -1)
+		}
+	}
 	if strings.HasSuffix(srcURL, "/") != strings.HasSuffix(dstURL, "/") {
 		logger.Fatalf("SRC and DST should both end with path separator or not!")
 	}
